c9924e9b5a56c1da087d893de2d5de61ab59123c
Misha Biryuk <vovann7093@gmail.com>
2021-10-27 13:38:03 +0300

    first commit

---
+++ Hello Misha/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Hello_Misha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Hello Misha";
+            Console.WriteLine(phrase);
+            char[] letters = phrase.ToCharArray();
+            for (int i = 6; i < letters.Length; i++)
+            {
+                Console.WriteLine(char.ToUpper(letters[i]));
+            }
+        }
+    }
+}
