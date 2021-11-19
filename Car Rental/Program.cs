b006cba2b55497c096fe3a07b6f32944338a4107
Misha Biryuk <vovann7093@gmail.com>
2021-11-19 15:36:54 +0200

    Car Rental

---
+++ Car Rental/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace Car_Rental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] carMark = {"Audi TT", "Audi RS", "BMW X7", "BMW X5", "Mercedec McLaren", "Opel ASTRA", "Chevrolet Camaro", "Chevrolet Impala", "Ford GT", "Ford Mustang", "Bugatti Veyron"};
+
+            string[] nameOfPeople = { "Andriy", "Misha", "Ivan", "Vlad", "Oleksandr", "Robert", "Mike", "Cris", "Evan", "Joel"};
+
+            Random random = new Random();
+            int customerCount = random.Next(1, 10);
+
+            var usersWithCars = new string[customerCount];
+
+            for (int i = 0; i < usersWithCars.Length; i++)
+            {
+                usersWithCars[i] = nameOfPeople[i] + " - " + carMark[i];
+            }
+            
+            int j = 0;
+            foreach (var item in usersWithCars)
+            {
+                Console.WriteLine(usersWithCars[j]);
+                j++;
+            }
+
+            Console.WriteLine($"User without cars: {nameOfPeople.Length - customerCount}");
+        }
+    }
+}
